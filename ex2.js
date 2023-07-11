156e776e760b77748370612833bd96b2a4c1475e
IvanGadjo <ivan.gadzovski@gmail.com>
2023-07-11 16:19:59 +0200

    Node Streams 2 - gzip stream with zlib - gunzip stream

--- ex2.js
+++ ex2.js
@@ -6,17 +6,18 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { Transform } from "stream";
+import zlib from "zlib";
 // import util from "util";
 
 let args = minimist(process.argv.slice(2), {
-  boolean: ["help", "in", "out"], // * in - get input from stdin
+  boolean: ["help", "in", "out", "compress", "uncompress"], // * in - get input from stdin
   string: ["file"],
 });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); // * custom __dirname, because it is not available in an ES module
 const BASEPATH = path.resolve(process.env.BASEPATH || __dirname); // * process.env.BASEPATH is a constant you pass when executing the script
-const OUTFILE = path.join(BASEPATH, "out.txt");
+let OUTFILE = path.join(BASEPATH, "out.txt");
 
 if (args.help) {
   printHelp();
@@ -31,14 +32,16 @@ if (args.help) {
 
 // *****
 function printHelp() {
-  console.log("Usage of ex1.js");
+  console.log("Usage of ex2.js");
   console.log("");
-  console.log("  ex1.js --help");
+  console.log("  ex2.js --help");
   console.log("");
-  console.log("--help     print out here help stuff");
-  console.log("--file      process the file");
-  console.log("--in, -    process stdin");
-  console.log("--out      print to stdout");
+  console.log("--help          print out here help stuff");
+  console.log("--file           process the file");
+  console.log("--in, -         process stdin");
+  console.log("--out           print to stdout");
+  console.log("--compress      compress output file");
+  console.log("--uncompress    uncompress input file");
 }
 
 function printErr(message, showHelp = false) {
@@ -59,8 +62,18 @@ function processFile(inStream) {
 
   outStream = outStream.pipe(upperCaseStream);
 
-  let targetStream;
+  if (args.compress) {
+    let gzipStream = zlib.createGzip();
+    outStream = outStream.pipe(gzipStream);
+    OUTFILE = String(OUTFILE) + ".gz";
+  }
 
+  if (args.uncompress) {
+    let unzippedStream = zlib.createGunzip();
+    outStream = outStream.pipe(unzippedStream);
+  }
+
+  let targetStream;
   if (args.out) {
     targetStream = process.stdout;
   } else {
